708da64acdd676c1fa496f37d5597111b7e685b0
Daryl <veruladm@ph.ibm.com>
2015-08-14 02:43:30 +0800

    Update about.html & contact.html with stylesheets

---
+++ app.js
@@ -0,0 +1,58 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+var cfenv = require('cfenv');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+app.engine('html',require('ejs').renderFile);
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+app.get("/",function(req,res){
+	res.render('index.html',null);
+});
+
+app.get("/about", function(req,res){
+	res.render('about.html',{title:'John'});
+});
+
+app.get("/about/:name?", function(req,res){
+	var name= req.params.name;
+	res.render('about.html',{title:name});
+});
+
+app.get("/contact", function(req,res){
+	res.render('contact.html',{title:'John'});
+});
+
+app.get("/contact/:name?", function(req,res){
+	var name = req.params.name;
+	res.render('contact.html',{title:name})
+})
+
+app.get("*", function(req,res){
+	res.send("Bad Route");
+})
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, function() {
+
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
